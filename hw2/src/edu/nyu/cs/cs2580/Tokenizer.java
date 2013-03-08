0c7bdf354fbf7a36048d2c6be9fdfeb31945a990
prasadkapde <pk1094@nyu.edu>
2013-03-08 16:09:22 -0500

    Adding Tokenizer

---
+++ hw2/src/edu/nyu/cs/cs2580/Tokenizer.java
@@ -0,0 +1,19 @@
+package edu.nyu.cs.cs2580;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Tokenizer {
+	public List<String> tokenize(String input){
+		List<String> tokens = new ArrayList<String>();
+		Pattern pattern = Pattern.compile("[^a-zA-Z0-9]");
+		Scanner scan = new Scanner(input);
+		scan.useDelimiter(pattern);
+	     while (scan.hasNext()) {
+	    	 tokens.add(scan.next());
+	     }
+	     return tokens;
+	}
+}
