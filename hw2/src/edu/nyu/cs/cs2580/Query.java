0e7a65249a21481a00bc66e8ca1bff6ca2b72fea
ashwath <aps398@nyu.edu>
2013-03-13 03:22:41 -0400

    Query.java updated

--- hw2/src/edu/nyu/cs/cs2580/Query.java
+++ hw2/src/edu/nyu/cs/cs2580/Query.java
@@ -1,5 +1,7 @@
 package edu.nyu.cs.cs2580;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.Scanner;
 import java.util.Vector;
 import java.util.regex.Pattern;
@@ -19,7 +21,14 @@ public class Query {
   public Vector<String> _tokens = new Vector<String>();
 
   public Query(String query) {
-    _query = query;
+    try {
+    	 query.toLowerCase();
+		_query = URLDecoder.decode(query, "UTF-8");
+	} catch (UnsupportedEncodingException e) {
+		// TODO Auto-generated catch block
+		System.out.println("URL DEcoder Error");
+		e.printStackTrace();
+	}
   }
 
   public void processQuery() {
